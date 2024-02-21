d366636548fd3aacfd389020bfe9be9b195af1ea
Fernando <ferelbue@gmail.com>
2024-02-21 22:03:49 +0100

    Se hacen reto 7

--- reto7.sql
+++ reto7.sql
@@ -1,71 +1,49 @@
-CREATE DATABASE reto_6;
-USE reto_6;
+CREATE DATABASE reto_7;
+USE reto_7;
 
 CREATE TABLE users(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(40),
     email VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    seller BOOLEAN,
+    buyer BOOLEAN,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE flights(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    number INT NOT NULL,
-	company VARCHAR(40) NOT NULL ,
-    destination VARCHAR(200) NOT NULL,
-    capacity INT NOT NULL,
-    date INT NOT NULL,
-    hour INT NOT NULL
-);
-
-
-CREATE TABLE flights_users(
+CREATE TABLE products(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(40) NOT NULL ,
     description VARCHAR(200) NOT NULL,
-    date INT NOT NULL,
-    hour INT NOT NULL,
-    room VARCHAR(40) NOT NULL ,
-    teacher VARCHAR(40) NOT NULL ,
+    price INT NOT NULL,
+    location VARCHAR(20) NOT NULL,
 
-    flight_id INT,
     user_id INT,
     
-    FOREIGN KEY (flight_id) REFERENCES flights(id),
     FOREIGN KEY (user_id) REFERENCES users(id) 
 );
 
-CREATE TABLE changes(
+
+CREATE TABLE orders(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    pice INT NOT NULL,
+    status ENUM('pending','in_process','done'),
+
+    user_id INT,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id) 
+);
+
+CREATE TABLE reviews(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(40) NOT NULL ,
     description VARCHAR(200) NOT NULL,
     date INT NOT NULL,
-    hour INT NOT NULL,
 
-    flight_id INT,
-    user_id INT,
+    seller_id INT,
+    buyer_id INT,
     
-    FOREIGN KEY (flight_id) REFERENCES flights(id),
-    FOREIGN KEY (user_id) REFERENCES users(id) 
+    FOREIGN KEY (seller_id) REFERENCES users(id),
+    FOREIGN KEY (buyer_id) REFERENCES users(id) 
 );
-
-CREATE TABLE reservations(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    seat VARCHAR(10) NOT NULL,
-    description VARCHAR(200)
-);
-
-CREATE TABLE seats(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	title VARCHAR(40) NOT NULL ,
-    description VARCHAR(200) NOT NULL,
-
-    flight_id INT,
-    reservation_id INT,
-    
-    FOREIGN KEY (flight_id) REFERENCES flights(id),
-    FOREIGN KEY (reservation_id) REFERENCES reservations(id) 
-);
-
